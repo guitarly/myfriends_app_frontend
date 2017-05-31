77554995fb18a3f2f321399aab62af615ae27bbd
025609 <matthewnguyen@C1MRQ046HF4H.fios-router.home>
2017-05-31 13:07:08 -0400

    fixed server.js

--- server.js
+++ server.js
@@ -2,7 +2,8 @@
 const express = require('express');
 const app = express();
 
-const port = 8000;
+// const port = 8000;
+const port = process.env.PORT || 8000;
 
 // =============== MIDDLEWARE ===================
 app.use(express.static('public'));
