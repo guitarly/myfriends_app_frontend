2cf812cbc9afebaec7f5644a5afbbf152229fd76
025609 <matthewnguyen@C1MRQ046HF4H.fios-router.home>
2017-05-31 13:02:14 -0400

    make it better

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const app = express();
 
-const port = 3050;
+const port = 8000;
 
 // =============== MIDDLEWARE ===================
 app.use(express.static('public'));
